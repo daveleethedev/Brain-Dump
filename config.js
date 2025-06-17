e023992aa457ccf9ffdf413884fb5f64f4c1af70
randonetheone <odavileal77@gmail.com>
2025-06-17 14:43:44 -0300

    comm

---
+++ config.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    icon: path.join(__dirname, 'assets', 'icon.ico'),
+    frame:true
+  });
+
+  win.loadFile('index.html');
+  win.maximize();
+  win.setMenu(null);
+   icon: path.join(__dirname, 'assets', 'icon.ico')
+}
+
+app.whenReady().then(createWindow);
